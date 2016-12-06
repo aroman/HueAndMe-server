5eee104e6d46a68c8b7c6c4b065b483869372677
Avi Romanoff <avi@romanoff.me>
2016-12-05 20:05:31 -0500

    add landing page and APK (!)

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 from pymongo import MongoClient
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+from flask import Flask, request, jsonify, render_template
+app = Flask(__name__, static_url_path='')
 
 PORT = os.getenv('PORT', 8000)
 MONGO_URI = os.getenv('MONGO_URI')
@@ -11,20 +11,13 @@ db = client['hue-and-me']
 
 @app.route('/')
 def index():
-    return 'nothing to see here :^)'
+    return render_template('index.html', scores=getSortedScores())
 
 @app.route('/scores', methods=['GET', 'POST'])
 def scores():
     if request.method == 'POST':
         return postScore(request)
-    return getScores(request)
-
-def getScores(request):
-    scores = []
-    for score in db.scores.find().sort('points', -1):
-        scores.append(dict(name=score['name'], points=score['points']))
-    response = jsonify(scores=scores)
-    return response
+    return jsonify(scores=getSortedScores())
 
 def postScore(request):
     content = request.get_json(force=True)
@@ -42,6 +35,12 @@ def statusResponse(text, status_code=200):
     response = jsonify(statusText=text)
     response.status_code = status_code
     return response
+
+def getSortedScores():
+    scores = []
+    for score in db.scores.find().sort('points', -1):
+        scores.append(dict(name=score['name'], points=score['points']))
+    return scores
 
 if __name__ == '__main__':
     app.run(port=PORT, debug=True)
